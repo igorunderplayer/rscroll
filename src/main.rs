a75043d3e8f9e1db6db51b94285b27b36b7f49e5
igorunderplayer <igorschug6@gmail.com>
2023-09-24 18:25:56 -0300

    Run command option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread::sleep, time::Duration};
+use std::{thread::sleep, time::Duration, process::Command};
 
 use clap::Parser;
 
@@ -13,22 +13,37 @@ struct Args {
     length: usize,
 
     #[arg(short, long, default_value_t = (" - ".to_string()))]
-    separator: String
+    separator: String,
+
+    #[arg(short, long, default_value_t = false)]
+    check_command: bool
 }
 
 fn main() {
     let args = Args::parse();
-
-
     let lenght = args.length;
-    let text = args.text;
     let delay = Duration::from_millis(args.delay);
     let sep = args.separator;
-    let chars: Vec<char> = text.chars().collect();
 
     let mut i = 0;
 
     loop {
+        let text = if args.check_command {
+            let yo = Command::new("sh")
+            .arg("-c")
+            .arg(&args.text)
+            .output()
+            .unwrap();
+    
+            let t = String::from_utf8(yo.stdout).unwrap();
+            t.trim().to_owned()
+        } else {
+            args.text.trim().to_owned()
+        };
+    
+        let chars: Vec<char> = text.chars().collect();
+
+
         if lenght >= chars.len() {
             println!("{}", text);
         } else {
